5e88b83a3a2e06a5c78686307eaa0f87f07ef992
Yelizar Sezon <sezon2004@gmail.com>
2023-01-03 11:13:12 -0800

    Project created

---
+++ eTicketsMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace eTicketsMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
